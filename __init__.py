0cd951f1ef94e210c51390d949e0148d3b743d67
Ivann78 <ivann.mouchard@free.fr>
2024-02-20 10:51:55 +0100

    update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
 from flask import Flask,render_template
+from flask import Flask, render_template, jsonify
+import json
+import sqlite3
 
 app = Flask(__name__) #creating flask app name
 
@@ -17,6 +20,19 @@ def resume_2():
 @app.route('/resume_template')
 def resume_template():
     return render_template("resume_template.html")
+
+# Création d'une nouvelle route pour la lecture de la BDD
+@app.route('/lecture/')
+def ReadBDD():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM livres').fetchall()
+    conn.close()
+
+    # Convertit la liste de livre en un format JSON
+    json_posts = [{'id': post['id'], 'title': post['title'], 'content': post['auteur']} for post in posts]
+
+    # Renvoie la réponse JSON
+    return jsonify(posts=json_posts)
 
 if(__name__ == "__main__"):
     app.run()
